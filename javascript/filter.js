29158e6034a73e5a5addffdbb1660e57375600c3
Wiktor <wikcioo@gmail.com>
2021-01-12 19:49:10 +0100

    finished filter functionality and added suble changes to project styles

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,6 +1,33 @@
-const projects = document.querySelectorAll('.image');
 const filter = document.querySelector('.filter-menu');
+const projects = document.querySelector('.project');
+
+const highlightActive = function(category) {
+    Array.from(filter.children).forEach(child => {
+        if (child.textContent === category) {
+            child.classList.add('active');
+        } else {
+            child.classList.remove('active');
+        }
+    });
+}
 
 filter.addEventListener('click', event => {
-    console.log(event.target.textContent);
+    showProjectsOf(event.target.textContent);
+    highlightActive(event.target.textContent)
 });
+
+const showProjectsOf = function(category) {
+    Array.from(projects.children).forEach(child => {
+        if (category === 'all') {
+            child.style.display = 'flex';
+            return;
+        }
+        if (!child.classList.contains(category)) {
+            child.style.display = 'none';
+        } else {
+            child.style.display = 'flex';
+        }
+    }
+)};
+
+highlightActive('all');
