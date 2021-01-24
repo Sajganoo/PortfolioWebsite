e03bc0dd02702ee4fd4748013994d851f20c40d2
Wiktor <wikcioo@gmail.com>
2021-01-24 11:57:12 +0100

    implemented filter behaviour

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,4 +1,5 @@
-const drop_btn = document.querySelector(".drop-btn span");
+const drop_btn = document.querySelector(".drop-btn");
+const drop_btn_arrow = document.querySelector(".drop-btn span");
 const tooltip = document.querySelector(".tooltip");
 
 const menu_wrapper = document.querySelector(".wrapper");
@@ -19,6 +20,11 @@ const iot_btn = document.querySelector(".back-iot-btn");
 drop_btn.onclick = () => {
     menu_wrapper.classList.toggle("show");
     tooltip.classList.toggle("show");
+    if (menu_wrapper.classList.contains("show")) {
+        drop_btn_arrow.classList = "fas fa-caret-down";
+    } else {
+        drop_btn_arrow.classList = "fas fa-caret-up";
+    }
 };
 
 programming_item.onclick = () => {
@@ -51,11 +57,35 @@ iot_btn.onclick = () => {
     iot_drop.style.display = "none";
 };
 
-const selectable = document.querySelectorAll('.selectable');
-console.log(selectable);
 
-// for (let i = 0; i < selectable.length; i++) {
-//     selectable[i].addEventListener('click', event => {
-        
-//     });
-// }
+const projects = document.querySelectorAll(".image");
+
+const sortProjects = function(name) {
+    if (name === "all") {
+        for (let i = 0; i < projects.length; i++) {
+            projects[i].style.display = "flex";
+        }
+    } else {
+        for (let i = 0; i < projects.length; i++) {
+            if (!projects[i].classList.contains(name)) {
+                projects[i].style.display = "none";
+            } else {
+                projects[i].style.display = "flex";
+            }
+        }    
+    }
+}
+
+
+const selectable = document.querySelectorAll('.selectable');
+
+for (let i = 0; i < selectable.length; i++) {
+    selectable[i].addEventListener('click', event => {
+        drop_btn.innerHTML = `${event.target.lastChild.textContent.trim()}<span class="fas fa-caret-down"></span>`;
+        sortProjects(event.target.lastChild.textContent.trim().toLowerCase().split(" ")[0]);
+        drop_btn.click();
+    });
+}
+
+
+
