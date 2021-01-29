470b3f51ae4522e6a1feea72ffcb7009e7c1e95d
Wiktor <wikcioo@gmail.com>
2021-01-29 20:22:21 +0100

    added theme variables swapping

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -20,17 +20,23 @@ window.onload = function() {
 
 const setTheme = function(mode) {
     if (mode == 'light') {
-        document.documentElement.style.setProperty('--animation-background', 'radial-gradient(#d6ecd2, #519839)');
-        document.documentElement.style.setProperty('--dark-blue', '#49852e');
-        document.documentElement.style.setProperty('--medium-blue', '#5aac44');
-        document.documentElement.style.setProperty('--light-blue', '#7bc86c'); 
-        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--animation-background', 'radial-gradient(#343c41, #191c1f)');
+        document.documentElement.style.setProperty('--dark-blue', '#343c41');
+        document.documentElement.style.setProperty('--medium-blue', 'rgba(159, 239, 0, 0.5)');
+        document.documentElement.style.setProperty('--light-blue', '#9acc14'); 
+        document.documentElement.style.setProperty('--text-color', 'white');
+        document.documentElement.style.setProperty('--icon-background', 'rgb(116, 173, 0)');
+        document.documentElement.style.setProperty('--skills-background', 'rgba(119, 179, 0, 0.5)');
+        document.documentElement.style.setProperty('--skills-background-hover', 'rgba(119, 200, 0, 0.6)');
     } else {
         document.documentElement.style.setProperty('--animation-background', 'radial-gradient(#485777, #2b2e3d)');
         document.documentElement.style.setProperty('--dark-blue', 'rgb(100, 125, 220)');
         document.documentElement.style.setProperty('--medium-blue', 'rgb(0, 129, 180)');
         document.documentElement.style.setProperty('--light-blue', 'rgb(39, 178, 233)');
         document.documentElement.style.setProperty('--text-color', 'white');
+        document.documentElement.style.setProperty('--icon-background', 'rgb(96, 200, 241)');
+        document.documentElement.style.setProperty('--skills-background', 'rgba(81, 160, 235, 0.3)');
+        document.documentElement.style.setProperty('--skills-background-hover', 'rgba(81, 160, 235, 0.5)');
     }
 }
 
